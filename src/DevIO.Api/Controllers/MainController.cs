103fe1edfe8b20ff43e55e616a04eb5c2e23b77d
Guilherme de Alvarenga <gui.alva50@gmail.com>
2019-12-02 23:00:56 -0300

    Iniciando Projeto
    Implementando DTO (ViewModels), Setup Controllers

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validação de notificações de erro
+
+        // validação de modelstate
+
+        // validação de operação de negócios
+    }
+
+    [Route("api/[fornecedores]")]
+    public class FornecedoresController : MainController
+    {
+
+        private readonly IFornecedorRepository _fornecedorRepository;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedores = await _fornecedorRepository.ObterTodos();
+
+            return Ok(fornecedores);
+        }
+    }
+}
