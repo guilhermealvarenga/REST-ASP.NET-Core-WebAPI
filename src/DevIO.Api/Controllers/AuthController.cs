97bd3d9f9e5d43384f391740a84517d00ebc6770
Guilherme de Alvarenga <guilherme.alvarenga@bludata.com.br>
2019-12-11 17:09:36 -0300

    Controller de Autenticação

---
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/conta")]
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotificador notificador,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notificador)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email
+            };
+
+            return CustomResponse(registerUser);
+        }
+    }
+}
